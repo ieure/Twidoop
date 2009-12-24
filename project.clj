d676f59bf3bdfc7ef84c19681883660c0519418d
Ian Eure <ian.eure@gmail.com>
2009-12-24 10:14:57 -0800

    Added files

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject twidoop "0.0.1"
+  :description "Read from the firehose, write to Hadoop"
+  :dependencies [[org.clojure/clojure "1.1.0-ALPHA-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojure/clojure-http-client "1.0.0-SNAPSHOT"]])
